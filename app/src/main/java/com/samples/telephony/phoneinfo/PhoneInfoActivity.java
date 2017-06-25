87994495582381e90980598cf36d3166d54aa941
java2019 <java_2019@mail.ru>
2017-06-25 14:04:57 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/telephony/phoneinfo/PhoneInfoActivity.java
@@ -0,0 +1,13 @@
+package com.samples.telephony.phoneinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_info);
+    }
+}
